490444af418b5d6798d16dba31d3913192c4fb38
Giovanni Rodriguez <hector.ramos@dportenis.com.mx>
2023-08-14 12:30:29 -0600

    Fragments en componentes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,1 +1,10 @@
-export const FirstApp = () => <div>FirstApp</div>;
+import { Fragment } from "react";
+
+export const FirstApp = () => {
+    return (
+        <Fragment>
+            <h1>Giovanni</h1>
+            <p>Algo</p>
+        </Fragment>
+    );
+};
