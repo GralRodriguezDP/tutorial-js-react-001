a5111836d03953ff54ec0a561b93012aff4f0fb9
Giovanni Rodriguez <hector.ramos@dportenis.com.mx>
2023-08-14 12:46:16 -0600

    Variables en componentes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,18 @@
-import { Fragment } from "react";
+
+const getResult = () => {
+    return 4 + 4;
+};
 
 export const FirstApp = () => {
+    const newMessage = 'Giovanni Rodriguez';
+    const newMessage2 = [1,2,3,4,5,6,7,8,9];
+    
     return (
-        <Fragment>
-            <h1>Giovanni</h1>
+        <>
+            <h1>{ newMessage }</h1>
+            <h1>{ newMessage2 }</h1>
+            <h1>{ getResult() }</h1>
             <p>Algo</p>
-        </Fragment>
+        </>
     );
 };
